719aea2941af8fe95bc0cf693c9c94351246edc9
Nhuttruong61 <Nhuttruong2261@gmail.com>
2023-06-14 16:24:01 +0700

    setup toast

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,43 @@ import "./App.css";
 import Header from "./components/Header";
 import { Routes, Route } from "react-router-dom";
 import routes from "../src/router";
+import { ToastContainer } from "react-toastify";
 function App() {
   return (
-    <Routes>
-      {routes.map((route) => {
-        const Page = route.page;
-        const isShowHeader = route.isShowHeader;
-        return (
-          <Route
-            key={route.path}
-            path={route.path}
-            element={
-              <>
-                {isShowHeader && <Header />}
-                <Page />
-              </>
-            }
-          />
-        );
-      })}
-    </Routes>
+    <div>
+      <Routes>
+        {routes.map((route) => {
+          const Page = route.page;
+          const isShowHeader = route.isShowHeader;
+          return (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={
+                <>
+                  {isShowHeader && <Header />}
+                  <Page />
+                </>
+              }
+            />
+          );
+        })}
+      </Routes>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      {/* Same as */}
+      <ToastContainer />
+    </div>
   );
 }
 
