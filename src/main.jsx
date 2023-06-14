fad441771abec0d3eeb13a3c23d26df360fa24ce
Nhuttruong61 <Nhuttruong2261@gmail.com>
2023-06-14 16:24:59 +0700

    setup toast

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+    <App />
     </BrowserRouter>
   </React.StrictMode>
 );
