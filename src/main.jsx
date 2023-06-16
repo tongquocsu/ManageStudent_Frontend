0cfc5e85a7349a89e7da2bb135968a47d8a0eaef
thuanphatt <thphatt@gmail.com>
2023-06-16 20:47:33 +0700

    cai dat route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "./utils/fontawesome"; // Import file fontawesome.js
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,16 +2,71 @@ import ForgotPassword from "../pages/ForgotPassword";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
 import NoPage from "../pages/NoPage";
+import ManagerUser from "../pages/Admin/ManagerUser";
+
+import AccountInfo from "../pages/Admin/Teacher/AccountInfo";
+import HomeTeacher from "../pages/Admin/Teacher/Home";
+import ClassList from "../pages/Admin/Teacher/ClassList";
+import ViewSchedule from "../pages/Admin/Teacher/ViewSchedule";
+import ParentMeeting from "../pages/Admin/Teacher/ParentMeeting";
+import Settings from "../pages/Admin/Settings";
+import Logout from "../pages/Logout";
+import News from "../pages/News";
 const routes = [
   {
-    path: "/",
+    path: "/admin",
     page: Home,
+    isShowHeader: true,
+  },
+  {
+    path: "/admin/news",
+    page: News,
+    isShowHeader: true,
+  },
+  {
+    path: "/admin/manager-user",
+    page: ManagerUser,
+    isShowHeader: true,
+  },
+  {
+    path: "/admin/teacher/home",
+    page: HomeTeacher,
+    isShowHeader: true,
+  },
+  {
+    path: "/admin/teacher/account-info",
+    page: AccountInfo,
+    isShowHeader: true,
+  },
+  {
+    path: "/admin/teacher/class-list",
+    page: ClassList,
+    isShowHeader: true,
+  },
+  {
+    path: "/admin/teacher/view-schedule",
+    page: ViewSchedule,
+    isShowHeader: true,
+  },
+  {
+    path: "/admin/teacher/parent-meeting",
+    page: ParentMeeting,
     isShowHeader: true,
   },
 
   {
     path: "/login",
     page: Login,
+    isShowHeader: false,
+  },
+  {
+    path: "/admin/settings",
+    page: Settings,
+    isShowHeader: true,
+  },
+  {
+    path: "/sign-up",
+    page: Logout,
     isShowHeader: false,
   },
 
