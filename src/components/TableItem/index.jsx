6877622a140f7085725c0626cdf9d8f79c7ffc63
thuanphatt <thphatt@gmail.com>
2023-06-16 20:49:22 +0700

    them model & table components

---
+++ src/components/TableItem/index.jsx
@@ -0,0 +1,50 @@
+import { Button, Table } from "antd";
+import { useState } from "react";
+
+function TableItem(props) {
+  // eslint-disable-next-line react/prop-types
+  const { data = [], columns = [] } = props;
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const start = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setSelectedRowKeys([]);
+      setLoading(false);
+    }, 1000);
+  };
+
+  const onSelectChange = (newSelectedRowKeys) => {
+    console.log("selectedRowKeys changed: ", newSelectedRowKeys);
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
+
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+  };
+
+  const hasSelected = selectedRowKeys.length > 0;
+
+  return (
+    <div>
+      <div style={{ marginBottom: 16 }}>
+        <Button
+          type="primary"
+          onClick={start}
+          disabled={!hasSelected}
+          loading={loading}
+        >
+          Reload
+        </Button>
+        <span style={{ marginLeft: 8 }}>
+          {hasSelected ? `Selected ${selectedRowKeys.length} items` : ""}
+        </span>
+      </div>
+      <Table rowSelection={rowSelection} columns={columns} dataSource={data} />
+    </div>
+  );
+}
+
+export default TableItem;
