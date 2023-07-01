bcb6ca4bdfd28d44910788a62b0d2241412b5f5c
thuanphatt <thphatt@gmail.com>
2023-07-01 09:54:36 +0700

    cap nhat duong dan cac page cua admin

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -13,40 +13,108 @@ function getItem(label, key, icon, children, type) {
   };
 }
 const itemsTop = [
-  getItem("Quản lý người dùng", "sub1", <FontAwesomeIcon icon="user" />),
-  getItem("Giáo viên", "sub2", <FontAwesomeIcon icon="chalkboard-user" />, [
-    getItem("Trang chủ", "5", <FontAwesomeIcon icon="house" />),
-    getItem("Thông tin tài khoản", "6", <FontAwesomeIcon icon="circle-info" />),
-    getItem("Danh sách các lớp", "7", <FontAwesomeIcon icon="list" />),
-    getItem("Xem lịch dạy", "8", <FontAwesomeIcon icon="calendar-days" />),
-    getItem("Lịch họp phụ huynh", "9", <FontAwesomeIcon icon="calendar" />),
+  getItem("Quản lý người dùng", "managerUser", <FontAwesomeIcon icon="user" />),
+  getItem(
+    "Giáo viên",
+    "mangerTeacher",
+    <FontAwesomeIcon icon="chalkboard-user" />,
+    [
+      getItem("Trang chủ", "teacherHome", <FontAwesomeIcon icon="house" />),
+      getItem(
+        "Thông tin tài khoản",
+        "teacherAccountInfo",
+        <FontAwesomeIcon icon="circle-info" />
+      ),
+      getItem(
+        "Danh sách các lớp",
+        "teacherClassList",
+        <FontAwesomeIcon icon="list" />
+      ),
+      getItem(
+        "Xem lịch dạy",
+        "teacherViewSchedule",
+        <FontAwesomeIcon icon="calendar-days" />
+      ),
+      getItem(
+        "Lịch họp phụ huynh",
+        "teacherParentMeeting",
+        <FontAwesomeIcon icon="calendar" />
+      ),
+    ]
+  ),
+  getItem("Kế toán", "managerAccountant", <FontAwesomeIcon icon="user-pen" />, [
+    getItem(
+      "Thống kế",
+      "accountantStatistic",
+      <FontAwesomeIcon icon="chart-simple" />
+    ),
+    getItem(
+      "Phiếu chi",
+      "accountantPayment",
+      <FontAwesomeIcon icon="money-check-dollar" />
+    ),
+    getItem(
+      "Phiếu thu",
+      "accountantRecipient",
+      <FontAwesomeIcon icon="file-invoice" />
+    ),
+    getItem("Báo cáo", "accountantReport", <FontAwesomeIcon icon="receipt" />),
   ]),
-  getItem("Kế toán", "sub3", <FontAwesomeIcon icon="user-pen" />, [
-    getItem("Thống kế", "10", <FontAwesomeIcon icon="chart-simple" />),
-    getItem("Phiếu chi", "11", <FontAwesomeIcon icon="money-check-dollar" />),
-    getItem("Phiếu thu", "12", <FontAwesomeIcon icon="file-invoice" />),
-    getItem("Báo cáo", "13", <FontAwesomeIcon icon="receipt" />),
-  ]),
-  getItem("Quản lý học vụ", "sub4", <FontAwesomeIcon icon="user-tie" />, [
-    getItem("Học sinh", "14", <FontAwesomeIcon icon="user-graduate" />),
-    getItem("Thời khóa biểu", "15", <FontAwesomeIcon icon="calendar-days" />),
-    getItem("Lớp học", "16", <FontAwesomeIcon icon="school" />),
-    getItem("Môn học", "17", <FontAwesomeIcon icon="book" />),
-  ]),
-  getItem("Tạo người dùng", "sub5", <FontAwesomeIcon icon="user-tie" />, [
-    getItem("Học sinh", "18", <FontAwesomeIcon icon="user-graduate" />),
-    getItem("Giáo viên", "19", <FontAwesomeIcon icon="calendar-days" />),
-    getItem("Kế toán", "20", <FontAwesomeIcon icon="school" />),
-    getItem("Quản lý học vụ", "21", <FontAwesomeIcon icon="book" />),
+  getItem(
+    "Quản lý học vụ",
+    "managerStaff",
+    <FontAwesomeIcon icon="user-tie" />,
+    [
+      getItem(
+        "Học sinh",
+        "staffStudent",
+        <FontAwesomeIcon icon="user-graduate" />
+      ),
+      getItem(
+        "Thời khóa biểu",
+        "staffSchedule",
+        <FontAwesomeIcon icon="calendar-days" />
+      ),
+      getItem("Lớp học", "staffClassroom", <FontAwesomeIcon icon="school" />),
+      getItem("Môn học", "staffSubject", <FontAwesomeIcon icon="book" />),
+    ]
+  ),
+
+  getItem("Tạo người dùng", "createUser", <FontAwesomeIcon icon="user-tie" />, [
+    getItem(
+      "Học sinh",
+      "createStudent",
+      <FontAwesomeIcon icon="user-graduate" />
+    ),
+    getItem(
+      "Giáo viên",
+      "createTeacher",
+      <FontAwesomeIcon icon="calendar-days" />
+    ),
+    getItem("Kế toán", "createAccountant", <FontAwesomeIcon icon="school" />),
+    getItem(
+      "Quản lý học vụ",
+      "createAccStaff",
+      <FontAwesomeIcon icon="book" />
+    ),
   ]),
 ];
+
 const itemsBottom = [
-  getItem("Cài đặt", "sub6", <FontAwesomeIcon icon="gear" />),
-  getItem("Đăng xuất", "sub7", <FontAwesomeIcon icon="right-to-bracket" />),
+  getItem("Cài đặt", "settings", <FontAwesomeIcon icon="gear" />),
+  getItem("Đăng xuất", "logout", <FontAwesomeIcon icon="right-to-bracket" />),
 ];
 
 // submenu keys of first level
-const rootSubmenuKeys = ["sub1", "sub2", "sub3", "sub4", "sub5", "sub6"];
+const rootSubmenuKeys = [
+  "managerUser",
+  "mangerTeacher",
+  "managerAccountant",
+  "managerStaff",
+  "createUser",
+  "settings",
+  "logout",
+];
 const Sidebar = () => {
   const navigate = useNavigate();
   const [openKeys, setOpenKeys] = useState(["sub1"]);
@@ -61,18 +129,26 @@ const Sidebar = () => {
   const handleMenuClick = (key) => {
     // Define route paths
     const routes = {
-      sub1: "/admin/manager-user",
-      sub5: "/settings",
-      sub6: "/sign-up",
-      5: "/admin/teacher/home",
-      6: "/admin/teacher/account-info",
-      7: "/admin/teacher/class-list",
-      8: "/admin/teacher/view-schedule",
-      9: "/admin/teacher/parent-meeting",
-      10: "/admin/accountant/statistic",
-      11: "/admin/accountant/payment",
-      12: "/admin/accountant/recipient",
-      13: "/admin/accountant/report",
+      managerUser: "/admin/manager-user",
+      settings: "/settings",
+      signUp: "/sign-up",
+      teacherHome: "/admin/teacher/home",
+      teacherAccountInfo: "/admin/teacher/account-info",
+      teacherClassList: "/admin/teacher/class-list",
+      teacherViewSchedule: "/admin/teacher/view-schedule",
+      teacherParentMeeting: "/admin/teacher/parent-meeting",
+      accountantStatistic: "/admin/accountant/statistic",
+      accountantPayment: "/admin/accountant/payment",
+      accountantRecipient: "/admin/accountant/recipient",
+      accountantReport: "/admin/accountant/report",
+      staffStudent: "/admin/acc-staff/student",
+      staffSchedule: "/admin/acc-staff/schedule",
+      staffClassroom: "/admin/acc-staff/classroom",
+      staffSubject: "/admin/acc-staff/subject",
+      createStudent: "/admin/create/student",
+      createTeacher: "/admin/create/teacher",
+      createAccountant: "/admin/create/accountant",
+      createAccStaff: "/admin/create/acc-staff",
     };
 
     // Get the corresponding route path based on the key
@@ -84,6 +160,11 @@ const Sidebar = () => {
     }
   };
 
+  // const handleLogout = (key) => {
+  //   console.log(key);
+  //   localStorage.removeItem("access_token");
+  //   Navigate("/login");
+  // };
   return (
     <div className="mx-auto">
       <Menu
